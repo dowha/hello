2a7b908c01feca53368502f6c836e9750def0d0f
Dowha Kim <mail@dowha.kim>
2025-02-13 23:34:58 +0900

    Feature: Update sitemap settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/sitemap.xml",
+          headers: [
+            {
+              key: "Content-Type",
+              value: "application/xml",
+            },
+          ],
+        },
+        {
+          source: "/sitemap_index.xml",
+          headers: [
+            {
+              key: "Content-Type",
+              value: "application/xml",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
